52967b729e430364ab4c8329af0d475748e64721
Siddhesh Pandit <siddheshpandit12@gmail.com>
2024-05-13 19:40:08 +0530

    database connection

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+console.log(process.env.DATABASE_STRING);
+mongoose.connect(process.env.DATABASE_STRING).then(()=>{
+    console.log("Connected");
+}).catch((error)=>{
+    console.log(error);
+});
 const app=express();
 
 app.listen(3000,()=>{
