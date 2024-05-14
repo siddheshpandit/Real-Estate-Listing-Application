a09db5515a3352725ae934382ee248840f289897
Siddhesh Pandit <siddheshpandit12@gmail.com>
2024-05-14 14:47:03 +0530

    Signup page ui

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.DATABASE_STRING,{
 const app=express();
 app.use(express.json());
 app.listen(process.env.PORT,()=>{
-    console.log("Server running on port 3000")
+    console.log(`Server running on port ${process.env.PORT}`)
 })
 
 app.use('/api/user',userRouter);
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-center font-semibold text-3xl  my-7'>Sign Up</h1>
+      <form className='flex flex-col gap-4'>
+        <input type="text" placeholder='Username' className='border p-3 rounded-md'/>
+        <input type="email" placeholder='Email' className='border p-3 rounded-md'/>
+        <input type="password" placeholder='Password' className='border p-3 rounded-md'/>
+        <button className='bg-slate-700 hover:opacity-90 p-3 rounded-md text-white uppercase disabled:opacity-80'>Sign up</button>
+      </form>
+      <div className='flex gap-2 mt-3'>
+        <p>Have an account?</p>
+        <Link to={'/signin'}><span className='text-blue-700'> Sign In</span></Link>
+      </div>
+    </div>
   )
 }
 
