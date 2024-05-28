5356a04b363951c47aa20fcafc9b936de2d4fce4
Siddhesh Pandit <siddheshpandit12@gmail.com>
2024-05-28 20:21:15 +0530

    user update api

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,5 +1,26 @@
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js"
+import bcryptjs from 'bcryptjs';
 export const test = (req,res)=>{
     res.json({
         message:'Hello World'
     })
+}
+
+export const updateUser = async (req, res, next) => {
+    const { username, password: plainPassword, email, avatar } = req.body;
+    if (req.user.id !== req.params.id) return next(errorHandler(401, 'Forbidden'));
+    try {
+        const updatedData={};
+        if(plainPassword){
+            updatedData.password = bcryptjs.hashSync(plainPassword,10);
+        }
+        if (username) updatedData.username = username;
+        if (email) updatedData.email = email;
+        if (avatar) updatedData.avatar = avatar;
+        const updatedUser = await User.findByIdAndUpdate(req.params.id,{$set:updatedData},{new:true})
+        const {password,...rest} = updatedUser._doc;
+        res.status(200).json(rest);
+    } catch (error) {
+    }
 }
--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 mongoose.connect(process.env.DATABASE_STRING,{
     dbName:'RealEstate-Listing'
@@ -17,6 +18,7 @@ app.use(cors({
     origin:'*'
 }))
 app.use(express.json());
+app.use(cookieParser());
 app.listen(process.env.PORT,()=>{
     console.log(`Server running on port ${process.env.PORT}`)
 })
